d714a1f140782cd036518a9b499e672f847b2d0e
ugoanyaegbunam <111075507+ugoanyaegbunam@users.noreply.github.com>
2024-10-22 15:12:05 -0500

    add debugging to script

--- scripts/rename_game.sh
+++ scripts/rename_game.sh
@@ -9,17 +9,18 @@ REPLACEMENT_PHRASE="$3"
 echo "Replacing '$PHRASE1' and '$PHRASE2' with '$REPLACEMENT_PHRASE'"
 
 # Replace the patterns in all files in the repository,
-# excluding the .git, script, and hooks directories.
+# excluding the .git, .github, scripts, and hooks directories.
 # -r flag for recursive, -i for in-place editing, -E for extended regex support
 find . -type f \
   -not -path "./.git/*" \
   -not -path "./.github/*" \
   -not -path "./scripts/*" \
   -not -path "./hooks/*" \
-  -exec sed -i -E "s/($PHRASE1|$PHRASE2)/$REPLACEMENT_PHRASE/g" {} +
+  -exec sed -i -E "s/($PHRASE1|$PHRASE2)/$REPLACEMENT_PHRASE/g" {} + \
+  -exec sh -c 'echo "Replaced in file: {}"' \;
 
 # Rename files that have the pattern in their filename,
-# excluding the .git, script, and hooks directories.
+# excluding the .git, .github, scripts, and hooks directories.
 for file in $(find . -type f \
   -name "*$PHRASE1*" \
   -not -path "./.git/*" \
@@ -27,7 +28,12 @@ for file in $(find . -type f \
   -not -path "./scripts/*" \
   -not -path "./hooks/*"); do
   newfile=$(echo "$file" | sed -E "s/($PHRASE1|$PHRASE2)/$REPLACEMENT_PHRASE/g")
-  mv "$file" "$newfile"
+  
+  # Only rename if the new filename is different
+  if [ "$file" != "$newfile" ]; then
+    mv "$file" "$newfile"
+    echo "Renamed file: '$file' to '$newfile'"
+  fi
 done 
 
 echo "Replaced all instances of '$PHRASE1' and '$PHRASE2' with '$REPLACEMENT_PHRASE' in file contents and filenames, ignoring specified directories."
