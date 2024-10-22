5345df88e6b278f2cb2512a99b5e275a2044b451
ugoanyaegbunam <111075507+ugoanyaegbunam@users.noreply.github.com>
2024-10-22 14:25:37 -0500

    working on script

--- hooks/pull_rename.sh
+++ hooks/pull_rename.sh
@@ -8,14 +8,14 @@ echo "Running post-merge hook for branch: $current_branch"
 # Conditional logic based on the branch
 if [ "$current_branch" == "ethan" ]; then
   echo "On ethan branch, running ethan-specific game rename..."
-  ./scripts/rename_game.sh 'CarletonSpades' 'CarletonSpadesB'
+  ./scripts/rename_game.sh 'CompsSpades' 'CompsSpadesB'
 elif [ "$current_branch" == "leo" ]; then
   echo "On leo branch, running leo-specific game rename..."
-  ./scripts/rename_game.sh 'CarletonSpades' 'CarletonSpadesA'
+  ./scripts/rename_game.sh 'CompsSpades' 'CompsSpadesA'
 elif [ "$current_branch" == "jon" ]; then
   echo "On jon branch, running jon-specific game rename..."
-  ./scripts/rename_game.sh 'CarletonSpades' 'CarletonSpadesC'
+  ./scripts/rename_game.sh 'CompsSpades' 'CompsSpadesC'
 elif [ "$current_branch" == "ugo" ]; then
   echo "On ugo branch, running ugo-specific game rename..."
-  ./scripts/rename_game.sh 'CarletonSpades' 'CarletonSpadesE'
+  ./scripts/rename_game.sh 'CompsSpades' 'CompsSpadesE'
 fi
