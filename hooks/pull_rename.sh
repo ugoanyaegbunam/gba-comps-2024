77d4e2b038c6b1462d420ec7bad54cf5ae000495
ugoanyaegbunam <anyaegbunamu@carleton.edu>
2024-10-24 04:48:47 +0000

    Automated replacement of 'CompsSpadesC' and 'CompsSpadesD' with 'CompsSpadesD' and their lowercase versions

--- hooks/pull_rename.sh
+++ hooks/pull_rename.sh
@@ -14,7 +14,7 @@ elif [ "$current_branch" == "leo" ]; then
   ./scripts/rename_game.sh 'CompsSpadesD' 'CompsSpadesE'
 elif [ "$current_branch" == "jon" ]; then
   echo "On jon branch, running jon-specific game rename..."
-  ./scripts/rename_game.sh 'CompsSpadesD' 'CompsSpadesC'
+  ./scripts/rename_game.sh 'CompsSpadesD' 'CompsSpadesD'
 elif [ "$current_branch" == "ugo" ]; then
   echo "On ugo branch, running ugo-specific game rename..."
   ./scripts/rename_game.sh 'CompsSpadesD' 'CompsSpadesD'
