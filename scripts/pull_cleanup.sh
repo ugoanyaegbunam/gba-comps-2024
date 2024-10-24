ac47c9e99fb15d4a68f5f061cb2b2b875fc7547f
ugoanyaegbunam <111075507+ugoanyaegbunam@users.noreply.github.com>
2024-10-24 14:07:22 -0500

    add lower case clean up

--- scripts/pull_cleanup.sh
+++ scripts/pull_cleanup.sh
@@ -7,19 +7,23 @@ echo "Running repository cleanup for '$dev'"
 if [ $dev == "ethan" ] 
 then 
     ./rename_game.sh "CompsSpades" "CompsSpadesE" "CompsSpadesB"
+    ./rename_game.sh "compsspades" "compsspadese" "compsspadesb"
 fi 
 
 if [ $dev == "leo" ] 
 then 
     ./rename_game.sh "CompsSpades" "CompsSpadesB" "CompsSpadesE"
+    ./rename_game.sh "compsspades" "compsspadesb" "compsspadese"
 fi 
 
 if [ $dev == "jonathan" ] 
 then 
     ./rename_game.sh "CompsSpades" "CompsSpadesD" "CompsSpadesC"
+    ./rename_game.sh "compsspades" "compsspadesd" "compsspadesc"
 fi 
 
 if [ $dev == "ugo" ] 
 then 
     ./rename_game.sh "CompsSpades" "CompsSpadesC" "CompsSpadesD"
+    ./rename_game.sh "compsspades" "compsspadesc" "compsspadesd"
 fi 
