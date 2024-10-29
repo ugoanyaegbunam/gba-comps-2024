221cde481854b729b1ecaa23122aa24128b3e835
PotatoJim <vilmjo54@gmail.com>
2024-10-29 14:40:47 -0500

    get updated scripts

---
+++ scripts/pull_cleanup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+dev="$1"
+
+echo "Running repository cleanup for '$dev'"
+
+if [ $dev == "ethan" ] 
+then 
+    ./scripts/rename_game.sh "CompsSpades" "CompsSpadesE" "CompsSpadesB" "."
+    ./scripts/rename_game.sh "compsspades" "compsspadese" "compsspadesb" "."
+fi 
+
+if [ $dev == "leo" ] 
+then 
+    ./scripts/rename_game.sh "CompsSpades" "CompsSpadesB" "CompsSpadesE" "."
+    ./scripts/rename_game.sh "compsspades" "compsspadesb" "compsspadese" "."
+fi 
+
+if [ $dev == "jonathan" ] 
+then 
+    ./scripts/rename_game.sh "CompsSpades" "CompsSpadesD" "CompsSpadesC" "."
+    ./scripts/rename_game.sh "compsspades" "compsspadesd" "compsspadesc" "."
+fi 
+
+if [ $dev == "ugo" ] 
+then 
+    ./scripts/rename_game.sh "CompsSpades" "CompsSpadesC" "CompsSpadesD" "."
+    ./scripts/rename_game.sh "compsspades" "compsspadesc" "compsspadesd" "."
+fi 
+
+if [ $dev == "main" ] 
+then 
+    ./scripts/rename_game.sh "CompsSpadesD" "CompsSpadesC" "CompsSpades" "."
+    ./scripts/rename_game.sh "CompsSpadesE" "CompsSpadesB" "CompsSpades" "."
+    ./scripts/rename_game.sh "compsspadesd" "compsspadesc" "compsspades" "."
+    ./scripts/rename_game.sh "compsspadese" "compsspadesb" "compsspades" "."
+fi 
