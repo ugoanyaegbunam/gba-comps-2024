b8ec193d3314840c179ee16fe4f1eee2fbed87c1
ugoanyaegbunam <111075507+ugoanyaegbunam@users.noreply.github.com>
2024-11-05 13:56:18 -0600

    update gameinfos to have 4 players

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -11,22 +11,22 @@
 
 $gameinfos = [
     // Name of the game in English (will serve as the basis for translation)
-    'game_name' => "My Great Game",
+    'game_name' => "Comps Spades D",
 
     // Game publisher (use empty string if there is no publisher)
-    'publisher' => 'My Publishing Company',
+    'publisher' => 'Free Domain',
 
     // Url of game publisher website
-    'publisher_website' => 'http://www.mypublishingcompany.com/',
+    'publisher_website' => 'N/A',
 
     // Board Game Geek ID of the publisher
-    'publisher_bgg_id' => 1234,
+    'publisher_bgg_id' => 0,
 
     // Board game geek ID of the game
     'bgg_id' => 592,
 
     // Players configuration that can be played (ex: 2 to 4 players)
-    'players' => [2, 3, 4],
+    'players' => [4],
 
     // Suggest players to play with this number of players. Must be null if there is no such advice, or if there is only one possible player configuration.
     // NB: the automatic lobby will try first the lowest number of players if this is not specified. So you _have to_ specify this parameter if the lowest player number is not compatible with the default options.
